d4b8ee4c06e361e1d5f4b64a684d364f93f1acb8
taigoti <taigoti@proton.me>
2026-06-25 14:44:03 -0300

    refactor to 'dynamic' problem values

--- app/quantitative/optimizer.py
+++ app/quantitative/optimizer.py
@@ -1,25 +1,31 @@
 from pulp import *
 from app.quantitative.exceptions import SolucaoInviavelError, SolucaoIlimitadaError
 
-def optimize_problem(x1Gain, x2Gain):
+def optimize_problem(data):
+    vars = data.get("variables")
+    gains = data.get("gain")
+    constraints = data.get("constraints")
     prob = LpProblem("Optimization_Problem", LpMaximize)
 
-    x1 = LpVariable("x1", lowBound=0, cat = "Integer")  # Variable x1
-    x2 = LpVariable("x2", lowBound=0, cat = "Continuous")  # Variable x2
+    variablesDict = dynamic_vars(vars)
+    x1 = variablesDict.get("x1")
+    x2 = variablesDict.get("x2")
+    prob += (gains[0] * x1) + (gains[1] * x2)  # Objective function
 
-    prob += (x1Gain * x1) + (x2Gain * x2)  # Objective function
-
-    prob += 2 *x1 + 4 * x2 <= 35  # Constraint 1
-    prob += 4 * x1 + 2 * x2 <= 42  # Constraint 2
+    prob += (constraints[0][0] * x1) + (constraints[0][1]) * x2 <= 35  # Constraint 1
+    prob += (constraints[1][0]) * x1 + (constraints[1][1] * x2) <= 42  # Constraint 2
 
     status = prob.solve()
     problem_status = LpStatus[status]  # Solve the problem
     checkStatus(problem_status)
 
     print(f"Status do Algoritmo: {problem_status}")
-    print(f"Quantidade Ótima do Produto A: {value(x1)}")
-    print(f"Quantidade Ótima do Produto B: {value(x2)}")
+
+    for x in variablesDict.keys():
+        print(f"Quantidade Ótima do {x}: {value(variablesDict[x])}")
+
     print(f"Lucro Máximo Possível: R$ {value(prob.objective)}")
+
 
 def checkStatus(status):
     if status == "Infeasible":
@@ -35,5 +41,28 @@ def checkStatus(status):
         from app.quantitative.exceptions import ModelizacaoError
         raise ModelizacaoError(f"O solver falhou com o status: {status}")
 
+
+def dynamic_vars(vars):
+    variablesDict = {}
+    k = 0
+
+    while k < len(vars):
+        key = vars[k]
+
+        value = LpVariable(vars[k], lowBound=0, cat="Continuous")
+        variablesDict[key] = value
+        k += 1
+
+    return variablesDict
+
+
 if __name__ == "__main__":
-    optimize_problem(5, 3)  # Exemplo de ganhos para x1 e x2
+    data = {
+        "variables": ["x1", "x2"],
+        "gain": [3, 5],
+        "constraints": [
+            [2, 1], [1, 2]
+        ]
+    }
+
+    optimize_problem(data)  # Exemplo de ganhos para x1 e x2
