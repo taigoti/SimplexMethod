34ef7e6c78e93aaa5c8afd72ee717e34bf857796
taigoti <taigoti@proton.me>
2026-07-20 20:33:02 -0300

    initializing streamlit

---
+++ app/models/web_view.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def initialize_page():
+    st.set_page_config(page_title="Simplex Solver", layout="centered")
+    st.title("Método Simplex")
+    st.write("Vamos iniciar! Preencha as informações seguindo o formato correto.")
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from app.models.data_format import set_expressions
 from app.quantitative.optimizer import optimize_problem
+from app.models.web_view import initialize_page
 
 if __name__ == "__main__":
-    print("=============================================\n")
-
-    print("Bem vindo à resolução com o Método Simplex!")
-    print("Vamos iniciar! Preencha as informações seguindo o formato correto.")
-
-    print("\n=============================================\n")
+    initialize_page()
 
     problem = set_expressions()
-
     optimize_problem(problem)
