8cd760f152a5bd037bcc914014ebb24a443d7416
taigoti <taigoti@proton.me>
2026-07-20 21:07:24 -0300

    feat: executing feedback

--- app/models/web_view.py
+++ app/models/web_view.py
@@ -1,12 +1,15 @@
 import streamlit as st
+import time
 
 def initialize_page():
     st.set_page_config(page_title="Simplex Solver", layout="centered")
     st.title("Método Simplex")
     st.write("Vamos iniciar! Preencha as informações seguindo o formato correto.")
 
-def executing():
-    pass
+def executing(optimize_problem, *args, **kwargs):
+    with st.spinner('Executando...'):
+        time.sleep(1)
+        optimize_problem(*args, **kwargs)
 
 def show_results(status, objective, variables, max_gain):
     st.write(status)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from app.models.data_format import set_expressions
 from app.quantitative.optimizer import optimize_problem
-from app.models.web_view import initialize_page
+from app.models.web_view import initialize_page, executing
 
 if __name__ == "__main__":
     initialize_page()
 
     problem = set_expressions()
-    optimize_problem(problem)
+    executing(optimize_problem, problem)
