9eb6ef860067560235e715b95129ebc494e12a6b
taigoti <taigoti@proton.me>
2026-07-25 00:29:08 -0300

    feat: start independent modules of the project

--- app/models/web_view.py
+++ app/models/web_view.py
@@ -8,7 +8,7 @@ def initialize_page():
 
 def executing(optimize_problem, *args, **kwargs):
     with st.spinner('Executando...'):
-        time.sleep(1)
+        time.sleep(2)
         optimize_problem(*args, **kwargs)
 
 def variables_num():
@@ -30,3 +30,22 @@ def show_results(status, objective, variables, max_gain):
     st.write(objective)
     st.write(variables)
     st.write(max_gain)
+
+
+def input_problem():
+    i = 1
+    cons_array = []
+
+    vars = st.number_input(
+        "Digite quantas variáveis o problema terá (ex: 2 = x1, x2): ",
+        min_value=2)
+
+    cons = st.number_input(
+        "Quantas restrições o problema tem? (desconsidere a restrição de não-negatividade) ",
+        min_value=1)
+
+    while i <= cons:
+        cons_array.append(st.text_input(f"Digite a {i}ª restrição: ", placeholder="ex: 3x1 + 0x2 + 1x3 <= 15"))
+        i += 1
+
+    print(vars, cons, cons_array)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from app.models.data_format import set_expressions
 from app.quantitative.optimizer import optimize_problem
-from app.models.web_view import initialize_page, executing
+from app.models.web_view import initialize_page, executing, input_problem
 
 if __name__ == "__main__":
     initialize_page()
+    input_problem()
 
-    problem = set_expressions()
-    executing(optimize_problem, problem)
+    #problem = set_expressions()
+    #executing(optimize_problem, problem)
